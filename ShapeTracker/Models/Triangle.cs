45eaa8fbbe44468929e947f4d3fddb79ad9bf73f
Kymani Stephens <kymanist00@gmail.com>
2023-05-01 15:51:34 -0700

    APIE: Encapsulation

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -2,6 +2,9 @@ namespace ShapeTracker.Models
 {
 public class Triangle 
 {
+
+    /* SETTING FIELDS TO PRIVATE IS BEST PRACTICE
+       Encapsulation (For Secure Data Flow) */
     private int _side1;  // updated
     private int _side2;  // updated
     private int _side3;  // updated
@@ -13,21 +16,49 @@ public class Triangle
       _side3 = length3;  // updated
     }
 
+    // GETTER METHODS
+    /* Getter methods follow the naming 
+       convention: Get + NameOfField in Pascal case. */
     public int GetSide1()
     {
         return _side1;
     }
 
-      public int GetSide2()
+    public int GetSide2()
     {
         return _side2;
     }
 
-      public int GetSide3()
+    public int GetSide3()
     {
         return _side3;
     }
 
+    // SETTER METHODS
+
+    /* only create a setter method if you need to set the 
+    value of the private field outside of the class it 
+    belongs to */
+
+    /* Setter methods follow the naming 
+       convention: Set + NameOfField in Pascal case. */
+    
+        public void SetSide1(int newSide)
+    {
+    _side1 = newSide;
+    }
+
+    public void SetSide2(int newSide)
+    {
+    _side2 = newSide;
+    }
+
+    public void SetSide3(int newSide)
+    {
+    _side3 = newSide;
+    }
+
+    // CHECKTYPE METHOD
     public string CheckType() 
     {
     
@@ -50,3 +81,7 @@ public class Triangle
     }
 }
 }
+
+/* From this point forward, all class 
+fields should be private and getter 
+and setter methods should be defined as needed. */
