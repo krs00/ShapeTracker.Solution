faec5f6941f43f6594410230f02bcc5d6abbc8e2
Kymani Stephens <kymanist00@gmail.com>
2023-05-01 14:56:55 -0700

    add CheckType class method logic

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -2,6 +2,35 @@ namespace ShapeTracker.Models
 { 
 public class Triangle 
 {
-    // CODE HERE
+    public int Side1;
+    public int Side2 = 4;
+    public int Side3;
+
+    public Triangle(int length1, int length2, int length3)
+    {
+        Side1 = length1;
+        Side2 = length2;
+        Side3 = length3;
+    }
+
+    public string CheckType() 
+    {
+    if ((Side1 > (Side2 + Side3)) || (Side2 > (Side1 + Side3)) || (Side3 > (Side1 + Side2)))
+    {
+        return "not a triangle";
+    } 
+    else if ((Side1 != Side2) && ((Side1 != Side3)) && ((Side2 != Side3))) 
+    {
+        return "scalene triangle";
+    }  
+    else if ((Side1 == Side2) && (Side1 == Side3)) 
+    { 
+        return "equilateral triangle";
+    } 
+    else 
+    {
+        return "isosceles triangle";
+    }
+    }
 }
 }
