c1d2e046c78d9bb209d365ecae3e5d2c5eeaec81
kymani stephens <kymanist00@gmail.com>
2023-05-02 15:59:08 -0700

    add console decoration beep and color

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -8,6 +8,10 @@ namespace ShapeTracker
   {
     static void Main()
     { 
+      Console.Beep();
+      Console.Beep();
+      Console.Beep();
+      Console.BackgroundColor = ConsoleColor.Red;
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
