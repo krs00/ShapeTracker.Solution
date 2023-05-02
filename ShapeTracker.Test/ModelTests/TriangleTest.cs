4ce9b0e85df17e58b406c131720c43715a4c00f2
Kymani Stephens <kymanist00@gmail.com>
2023-05-01 19:44:04 -0700

    test passes

--- ShapeTracker.Test/ModelTests/TriangleTest.cs
+++ ShapeTracker.Test/ModelTests/TriangleTest.cs
@@ -6,6 +6,17 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-    // Tests will go here!
+
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle();
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+
   }
 }
+
+
+/* The naming convention used for test methods is the following: 
+public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue() */
