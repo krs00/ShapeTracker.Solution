3d6879a49a62201c25b01e6595833c566ee14eeb
Kymani Stephens <kymanist00@gmail.com>
2023-05-01 20:11:32 -0700

    add Side2 get functionality + passing test

--- ShapeTracker.Test/ModelTests/TriangleTest.cs
+++ ShapeTracker.Test/ModelTests/TriangleTest.cs
@@ -6,11 +6,10 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle(2); 
+      Triangle newTriangle = new Triangle(2, 3); // Updated!
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
@@ -19,18 +18,18 @@ namespace ShapeTracker.Tests
     {
       // Arrange
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1);
+      Triangle newTriangle = new Triangle(length1, 2); // Updated!
       // Act
       int result = newTriangle.Side1;
-      // Assert 
+      // Assert
       Assert.AreEqual(length1, result);
     }
 
-        [TestMethod]
+    [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
       // Arrange
-      Triangle newTriangle = new Triangle(3);
+      Triangle newTriangle = new Triangle(3,3); // Updated!
       int newLength1 = 44;
       // Act
       newTriangle.Side1 = newLength1;
@@ -38,25 +37,16 @@ namespace ShapeTracker.Tests
       Assert.AreEqual(newLength1, newTriangle.Side1);
     }
 
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      // Arrange
+      int length2 = 3;
+      Triangle newTriangle = new Triangle(2, length2); // Updated!
+      // Act
+      int result = newTriangle.Side2;
+      // Assert
+      Assert.AreEqual(length2, result);
+    }
   }
 }
-
-
-/* The naming convention used for test methods is the following: 
-public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue() */
-
-
-
-/* Arrange, Act, Assert
-This test is more complex than our previous tests. Thankfully, there's an easy organizational 
-trick that helps demystify this process and keep our tests clean. It's called "Arrange, Act, Assert". With this 
-organizational method, we break our tests down into three stages:
-
-Arrange: We gather, declare and create all necessary components for the test.
-In the test above, we create length1 and newTriangle.
-
-Act: We invoke the functionality we're testing, often by calling a 
-method or retrieving a property. In the test above, we call newTriangle.Side1;.
-
-Assert: We confirm the functionality works as anticipated by comparing its 
-actual output with the anticipated output. This is when we call Assert.AreEqual().   */
